4bd233a7854dda1f8413806c3d1b291c6841aba3
Jim Downhower <jdownhower@gmail.com>
2016-05-21 17:02:22 -0400

    Implement Student

--- CourseManager/src/my_package/users/Student.java
+++ CourseManager/src/my_package/users/Student.java
@@ -3,6 +3,8 @@
  */
 package my_package.users;
 
+import java.util.ArrayList;
+
 import my_package.courses.Course;
 
 /**
@@ -10,6 +12,27 @@ import my_package.courses.Course;
  *
  */
 public class Student extends User {
+	/** Courses enrolled in */
+	private ArrayList<Course> courses;
+	/** Maximum number of credits student is eligible to enroll in */
+	private int maxCredits;
+	/** Maximum number of credits any student can enroll in */
+	public static final int MAX_CREDITS = 18;
+	
+	
+	/**
+	 * @param firstName
+	 * @param lastName
+	 * @param id
+	 * @param email
+	 * @param password
+	 * @param maxCredits
+	 */
+	public Student(String firstName, String lastName, String id, String email, String password,	int maxCredits) {
+		super(firstName, lastName, id, email, password);
+		courses = new ArrayList<Course>();
+		setMaxCredits(maxCredits);
+	}
 
 	/**
 	 * @param firstName
@@ -19,8 +42,24 @@ public class Student extends User {
 	 * @param password
 	 */
 	public Student(String firstName, String lastName, String id, String email, String password) {
-		super(firstName, lastName, id, email, password);
-		// TODO Auto-generated constructor stub
+		this(firstName, lastName, id, email, password, MAX_CREDITS);
+	}
+
+	/**
+	 * @return the maxCredits
+	 */
+	public int getMaxCredits() {
+		return maxCredits;
+	}
+
+	/**
+	 * @param maxCredits the maxCredits to set
+	 */
+	public void setMaxCredits(int maxCredits) {
+		if (maxCredits < 0 || maxCredits > MAX_CREDITS) {
+			throw new IllegalArgumentException();
+		}
+		this.maxCredits = maxCredits;
 	}
 
 	/* (non-Javadoc)
